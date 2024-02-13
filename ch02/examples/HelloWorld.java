8d6c803e624c601d60eadfb7390ecfe2e9090ade
Marcel-je <marcel.janssen.emami@exxeta.com>
2024-02-13 15:52:55 +0100

    Print & println tester

--- ch02/examples/HelloWorld.java
+++ ch02/examples/HelloWorld.java
@@ -5,6 +5,10 @@ package ch02.examples;
  */
 public class HelloWorld {
   public static void main( String[] args ) {
-    System.out.println("Hello, world!");
+    System.out.println("Hello, world!!!!!");
+    System.out.print("Test 1 ");
+    System.out.print("Test 2 ");
+    System.out.println("Test 3ln ");
+    System.out.println("Test newline ");
   }
 }
